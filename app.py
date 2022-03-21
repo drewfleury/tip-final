800ab60470ad1fb105bfc3cffd1d4fed2b1f138e
Drew Fleury <drewfleury@Drews-MacBook-Pro.local>
2022-03-21 19:19:02 +0100

    heroku deploy 2.0

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ TO DO:
 2. fix deployment in App ENgine
 3. Drill down KPI's and make more charts
 4. IF TIME add something to do with NLP
+
+THEMES: https://bootswatch.com/
 """
 
 # CHANGE SCALE OF Y AXIS TO LOG SCALE
@@ -11,15 +13,17 @@ TO DO:
 from pandas import value_counts
 from bq_api import *
 from dash import Dash, dcc, html, Input, Output, dash_table
+import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import plotly.express as px
 
-app = Dash(__name__) # NEED TO FIGURE OUT HOW TO DEPLOY USING gunicorn
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server=app.server
 
 
 app.layout = html.Div([
-    html.H1("Dash test 1"),
+    html.H1("Term Integration Project: KPIs", style={'textAlign': 'center'}),
+    html.H3("Select between raised, closed, and backloged"),
     dcc.Dropdown(
         id="priority_drop",
         options= [
@@ -28,34 +32,38 @@ app.layout = html.Div([
             {"value": "Backlog", "label": "Backlog"}
         ]
     ),
-    dcc.Graph(
+    html.Div([
+        dcc.Graph(
         id="scatter_chart_raised",
         figure = {
             'data' : [
                 go.Bar( #this allows there to be a graph from a set dataframe
                     x = df_priority_raised.priority,
-                    y = df_priority_raised.Count
+                    y = df_priority_raised.Count,
                 )
             ],
             'layout' : {
-                'title' : "Number of issues raised per priority level"
+                'title' : "Number of issues raised per priority level",
+                'type' : 'log'
             }
         }
-    ),
-    dcc.Graph( # for average resolution time divided into priority
-        id="avg_reso_time_raised",
-        figure = {
-            'data' : [
-                go.Bar( #this allows there to be a graph from a set dataframe
-                    x = Average_res_time_raised.Priority,
-                    y = Average_res_time_raised.AVE_Resolution_time_hours
-                )
-            ],
-            'layout' : {
-                'title' : "Average resolution time by priority level"
+        ),
+        dcc.Graph( # for average resolution time divided into priority
+            id="avg_reso_time_raised",
+            figure = {
+                'data' : [
+                    go.Bar( #this allows there to be a graph from a set dataframe
+                        x = Average_res_time_raised.Priority,
+                        y = Average_res_time_raised.AVE_Resolution_time_hours
+                    )
+                ],
+                'layout' : {
+                    'title' : "Average resolution time by priority level"
+                }
             }
-        }
-    ),
+        ),
+    ]), # , style={'display': 'flex', 'flex-direction': 'row'}
+    
     dcc.Dropdown(
         id="dropdown",
         options= [
@@ -66,12 +74,20 @@ app.layout = html.Div([
     html.H2(id="id-changes"),
     dash_table.DataTable(issue_cause_raised.to_dict('records'),[{"name": i, "id": i} for i in issue_cause_raised.columns], id='tbl'),
 ])
-# NEED TO FIGURE OUT WHY THIS CALLBACK IS CAUSING PAGE TO FREEZE
+
+scatter_chart_raised_layout = {
+                'title' : "Number of issues raised per priority level"
+            }
+avg_reso_time_raised_layout = {
+                'title' : "Average resolution time by priority level"
+            }
+
+# callback for number of issues raised per priority
 @app.callback(
     Output(component_id="scatter_chart_raised", component_property="figure"),
     Input(component_id="priority_drop", component_property="value")
 )
-def update_data(value):
+def update_data_num_issues_raised(value):
     if value == 'Raised':
         figure = {
             'data' : [
@@ -79,7 +95,8 @@ def update_data(value):
                     x = df_priority_raised.priority,
                     y = df_priority_raised.Count
                 )
-            ]
+            ],
+            'layout' : scatter_chart_raised_layout
         }
         return figure
     elif value == 'Closed':
@@ -89,7 +106,8 @@ def update_data(value):
                     x = df_priority_closed.priority,
                     y = df_priority_closed.Count
                 )
-            ]
+            ],
+            'layout' : scatter_chart_raised_layout
         }
     else:
         figure = {
@@ -98,9 +116,50 @@ def update_data(value):
                     x = df_priority_backlog.priority,
                     y = df_priority_backlog.Count
                 )
-            ]
+            ],
+            'layout' : scatter_chart_raised_layout
         }
     return figure
+
+# callback for average resolution time graph
+@app.callback(
+    Output(component_id="avg_reso_time_raised", component_property="figure"),
+    Input(component_id="priority_drop", component_property="value")
+)
+def update_data(value):
+    if value == 'Raised':
+        figure = {
+            'data' : [
+                go.Bar( #this allows there to be a graph from a set dataframe
+                    x = Average_res_time_raised.Priority,
+                    y = Average_res_time_raised.AVE_Resolution_time_hours
+                )
+            ],
+            'layout' : avg_reso_time_raised_layout
+        }
+        return figure
+    elif value == 'Closed':
+        figure = {
+            'data' : [
+                go.Bar( #this allows there to be a graph from a set dataframe
+                    x = Average_res_time_closed.Priority,
+                    y = Average_res_time_closed.AVE_Resolution_time_hours
+                )
+            ],
+            'layout' : avg_reso_time_raised_layout
+        }
+    else:
+        figure = {
+            'data' : [
+                go.Bar( #this allows there to be a graph from a set dataframe
+                    x = Average_res_time_backlog.Priority,
+                    y = Average_res_time_backlog.AVE_Resolution_time_hours
+                )
+            ],
+            'layout' : avg_reso_time_raised_layout
+        }
+    return figure
+#_______
 
 @app.callback(
     Output(component_id="id-changes", component_property="children"),
@@ -117,4 +176,4 @@ def update_bar_chart(value):
 
 #app.run_server(debug=True)
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
--- bq_api.py
+++ bq_api.py
@@ -28,9 +28,11 @@ df_priority_closed = query_to_df('SELECT priority, COUNT(priority) AS Count FROM
 df_priority_backlog = query_to_df('SELECT priority, COUNT(priority) AS Count FROM tip_dataset_1.backlog GROUP BY priority;')
 
 Average_res_time_raised = query_to_df('SELECT Priority, AVG(TIMESTAMP_DIFF(Resolution_Date_Time, Create_Date_Time, MINUTE)/60) AS AVE_Resolution_time_hours FROM tip_dataset_1.raised GROUP BY Priority')
+Average_res_time_closed = query_to_df('SELECT Priority, AVG(TIMESTAMP_DIFF(Resolution_Date_Time, Creation_Date_Time, MINUTE)/60) AS AVE_Resolution_time_hours FROM tip_dataset_1.closed GROUP BY Priority')
+Average_res_time_backlog = query_to_df('SELECT Priority, AVG(TIMESTAMP_DIFF(Resolution_Date_Time, Creation_Date_Time, MINUTE)/60) AS AVE_Resolution_time_hours FROM tip_dataset_1.backlog GROUP BY Priority')
 
 issue_cause_raised = query_to_df('SELECT Inc__Type, COUNT(Inc__Type) AS Incident_Count FROM tip_dataset_1.raised GROUP BY Inc__Type ORDER BY Incident_Count DESC')
 
-df_priority_raised
+
  
 # %%
